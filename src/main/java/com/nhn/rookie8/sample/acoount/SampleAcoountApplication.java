33b3663ed7342756466165eb6a944c9682b90d0c
박경원 <pkw1012@nhn.com>
2021-02-08 16:29:38 +0900

    created hello world project

---
+++ src/main/java/com/nhn/rookie8/sample/acoount/SampleAcoountApplication.java
@@ -0,0 +1,13 @@
+package com.nhn.rookie8.sample.acoount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAcoountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAcoountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhn/rookie8/sample/acoount/SampleAcoountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhn.rookie8.sample.acoount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAcoountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
